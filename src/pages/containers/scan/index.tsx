58bf63970dff2d1b1356b8221828182124b376ab
Joe Toeniskoetter <jtoeniskoetter.jt@gmail.com>
2023-08-29 10:42:09 -0500

    fixing linting errors

--- src/pages/containers/scan/index.tsx
+++ src/pages/containers/scan/index.tsx
@@ -1,20 +1,18 @@
 import React, { useState } from "react";
 import { QrScanner } from "@yudiel/react-qr-scanner";
 
-interface ScanProps {}
-
-const Scan: React.FC<ScanProps> = ({}) => {
-  const [result, setResult] = useState<any>();
-  function handleScan(data) {
-    console.log(data);
-  }
-  function handleError(err) {
-    console.error(err);
-  }
+const Scan: React.FC = ({}) => {
+  // const [result, setResult] = useState<{}}>();
+  // function handleScan(data) {
+  //   console.log(data);
+  // }
+  // function handleError(err) {
+  //   console.error(err);
+  // }
   return (
     <div>
-      <QrScanner onError={handleError} onDecode={handleScan} />
-      <p>{result}</p>
+      {/* <QrScanner onError={handleError} onDecode={handleScan} /> */}
+      <p>Scan</p>
     </div>
   );
 };
